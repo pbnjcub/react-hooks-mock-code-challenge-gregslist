8cfa2a0a598b6d50d860ad29556ecc0fe334645d
Paul Jung <pbnjcub@gmail.com>
2022-09-12 20:00:02 -0400

    commit tree ba2c2f23f6f1a3e33e129d7acb92854f1a60e2e1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [items, setItems] = useState([])
+  const [search, setSearch] = useState(" ")
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+    .then((resp) => resp.json())
+    .then((items) => setItems(items))
+  }, [])
+
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter((filter) => filter.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+
+  function handleSearch(searchedItem) {
+    const updatedItems = items.filter((item) => {
+      if (searchedItem === " ") {
+        return item
+      } else {
+        return item.description.toLowerCase().match(searchedItem)
+      }
+    })
+    setItems(updatedItems)
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={handleSearch} setSearch={setSearch} search={search} />
+      <ListingsContainer items={items} onDeleteItem={handleDeleteItem} />
     </div>
   );
 }
